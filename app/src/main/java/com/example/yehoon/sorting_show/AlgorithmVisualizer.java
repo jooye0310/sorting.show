8a679eaff43ecc711cc7e96913c1ed8580fd9de5
jester9108 <kimavi@bc.edu>
2018-11-29 16:40:20 -0500

    Algorithm Visualizer made by Naman Dwivedi

---
+++ app/src/main/java/com/example/yehoon/sorting_show/AlgorithmVisualizer.java
@@ -0,0 +1,57 @@
+package com.example.yehoon.sorting_show;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+
+public abstract class AlgorithmVisualizer extends View {
+
+    public AlgorithmVisualizer(Context context) {
+        super(context);
+
+    }
+
+    public AlgorithmVisualizer(Context context, AttributeSet atrrs) {
+        super(context, atrrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getDimensionInPixel(330));
+    }
+
+    public int getDimensionInPixel(int dp) {
+        int density = getResources().getDisplayMetrics().densityDpi;
+
+        int modifieddp = dp;
+        switch (density) {
+            case DisplayMetrics.DENSITY_LOW:
+                modifieddp = dp - dp / 2;
+                break;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                modifieddp = dp - dp / 3;
+                break;
+            case DisplayMetrics.DENSITY_HIGH:
+                modifieddp = dp - dp / 4;
+                break;
+            case DisplayMetrics.DENSITY_XHIGH:
+            case DisplayMetrics.DENSITY_XXHIGH:
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                modifieddp = dp;
+                break;
+            default:
+                break;
+        }
+
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, modifieddp, getResources().getDisplayMetrics());
+    }
+
+    public int getDimensionInPixelFromSP(int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
+    }
+
+    public abstract void onCompleted();
+}
